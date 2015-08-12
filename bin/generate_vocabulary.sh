75c22e830d50e9a69bc4882f0ebdb407a716c328
Kelly Davis <kdavis@mozilla.com>
2015-08-12 14:01:24 +0200

    Doubled word and count record allocation for largest corpora

--- bin/generate_vocabulary.sh
+++ bin/generate_vocabulary.sh
@@ -3,10 +3,10 @@
 { text2wfreq < language-models/pre-processed/ca/wiki.txt | wfreq2vocab > language-models/language-model/ca/wiki.vocab; } &
 { text2wfreq < language-models/pre-processed/cs/wiki.txt | wfreq2vocab > language-models/language-model/cs/wiki.vocab; } &
 { text2wfreq < language-models/pre-processed/cy/wiki.txt | wfreq2vocab > language-models/language-model/cy/wiki.vocab; } &
-{ text2wfreq < language-models/pre-processed/de/wiki.txt | wfreq2vocab -records 6000000 > language-models/language-model/de/wiki.vocab; } &
-{ text2wfreq < language-models/pre-processed/en/wiki.txt | wfreq2vocab -records 6000000 > language-models/language-model/en/wiki.vocab; } &
-{ text2wfreq < language-models/pre-processed/en-gb/wiki.txt | wfreq2vocab -records 6000000 > language-models/language-model/en-gb/wiki.vocab; } &
-{ text2wfreq < language-models/pre-processed/en-za/wiki.txt | wfreq2vocab -records 6000000 > language-models/language-model/en-za/wiki.vocab; } &
+{ text2wfreq < language-models/pre-processed/de/wiki.txt | wfreq2vocab -records 12000000 > language-models/language-model/de/wiki.vocab; } &
+{ text2wfreq < language-models/pre-processed/en/wiki.txt | wfreq2vocab -records 12000000 > language-models/language-model/en/wiki.vocab; } &
+{ text2wfreq < language-models/pre-processed/en-gb/wiki.txt | wfreq2vocab -records 12000000 > language-models/language-model/en-gb/wiki.vocab; } &
+{ text2wfreq < language-models/pre-processed/en-za/wiki.txt | wfreq2vocab -records 12000000 > language-models/language-model/en-za/wiki.vocab; } &
 { text2wfreq < language-models/pre-processed/es/wiki.txt | wfreq2vocab -records 6000000 > language-models/language-model/es/wiki.vocab; } &
 { text2wfreq < language-models/pre-processed/es-ar/wiki.txt | wfreq2vocab -records 6000000 > language-models/language-model/es-ar/wiki.vocab; } &
 { text2wfreq < language-models/pre-processed/es-cl/wiki.txt | wfreq2vocab -records 6000000 > language-models/language-model/es-cl/wiki.vocab; } &
